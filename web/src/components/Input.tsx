8d57a5923c01c2353a8d215926b326e99b130a08
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-16 11:43:29 +0200

    setting input

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -6,15 +6,15 @@ export function Input({ legend, ...rest }: Props) {
   return (
     <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
       {legend && (
-        <legend className="uppercase text-xxs  mb-2 text-inherits">
+        <legend className="uppercase text-xxs  mb-2 text-inherit">
           {legend}
         </legend>
       )}
 
       <input
         type="text"
+        className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent mb-2 outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
         {...rest}
-        className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent"
       />
     </fieldset>
   );
--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -2,20 +2,15 @@ import { Input } from "../components/Input";
 
 export function SignIn() {
   return (
-    <form action="" className="w-full flex-col gap-4">
+    <form action="" className="w-full flex flex-col gap-4">
       <Input
         required
         legend="Email"
         type="email"
         placeholder="youremail@email.com"
       />
-      
-      <Input
-        required
-        legend="Password"
-        type="password"
-        placeholder="youremail@email.com"
-      />
+
+      <Input required legend="Password" type="password" placeholder="123456" />
     </form>
   );
 }
