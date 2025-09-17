3d555cc75667d1c39f0dea40492722b3b8ec4559
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-17 16:53:10 +0200

    Update SignIn.tsx

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,19 +1,40 @@
+import type React from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useState } from "react";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsloading] = useState(false);
+
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    console.log(email, password);
+  }
   return (
-    <form action="" className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="Email"
         type="email"
         placeholder="youremail@email.com"
+        onChange={(event) => setEmail(event.target.value)}
       />
 
-      <Input required legend="Password" type="password" placeholder="123456" />
+      <Input
+        required
+        legend="Password"
+        type="password"
+        placeholder="123456"
+        onChange={(event) => setPassword(event.target.value)}
+      />
 
-      <Button>Enter</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Enter
+      </Button>
+
+      <a href="/signup">Create account</a>
     </form>
   );
 }
