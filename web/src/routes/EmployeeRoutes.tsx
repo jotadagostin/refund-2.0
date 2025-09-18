ba2d961e6acab8b78d068c32d5b52b5d844d3a29
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-18 16:58:41 +0200

    new page and route

---
+++ web/src/routes/EmployeeRoutes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router";
+import { Refund } from "../pages/Refund";
+import { NotFound } from "../pages/Notfound";
+
+export function EmployeeRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Refund />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
