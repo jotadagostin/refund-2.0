e221db2d049f4699feaa2e5fd4e85ed8464af9da
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-02 16:44:23 +0200

    update context

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,19 +1,23 @@
 import logoSvg from "../assets/logo.svg";
 import logoutSvg from "../assets/logout.svg";
+import { useAuth } from "../hooks/useAuth";
 
 export function Header() {
+  const auth = useAuth();
+
   return (
     <header className="w-full flex justify-between">
       <img src={logoSvg} alt="logo" className="my-8" />
 
       <div className="flex items-center gap-3 justify-around">
         <span className="text-sm font-semibold text-gray-200">
-          Hello, User!
+          Hello, {auth.session?.user.name}
         </span>
         <img
           src={logoutSvg}
           alt="Logout icon"
           className="my-8 cursor-pointer hover:opacity-75 transition ease-linear"
+          onClick={() => auth.remove()}
         />
       </div>
     </header>
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -5,18 +5,8 @@ import { ManagerRoutes } from "./ManagerRoutes";
 import { Loading } from "../components/Loading";
 import { useAuth } from "../hooks/useAuth";
 
-const isLoading = false;
-// const session = undefined;
-
-const session = {
-  user: {
-    role: "",
-  },
-};
-
 export function Routes() {
-  const context = useAuth();
-  console.log(context);
+  const { session, isLoading } = useAuth();
 
   function Route() {
     switch (session?.user.role) {
