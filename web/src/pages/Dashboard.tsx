c62ee1b4d7239a61ddbd3675d2059b97f739aaee
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-03 16:54:36 +0200

    Update Dashboard.tsx

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -45,6 +45,11 @@ export function Dashboard() {
     }
   }
 
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    fetchRefunds();
+  }
+
   function handlePagination(action: "next" | "previous") {
     setPage((prevPage) => {
       if (action === "next" && prevPage < totalOfPage) {
@@ -61,14 +66,14 @@ export function Dashboard() {
 
   useEffect(() => {
     fetchRefunds();
-  }, []);
+  }, [page]);
 
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitations</h1>
 
       <form
-        onSubmit={fetchRefunds}
+        onSubmit={onSubmit}
         className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input
